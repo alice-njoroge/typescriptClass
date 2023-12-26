b77ef0b72ae22b40a8155aae796d2200b0712701
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-26 22:10:21 +0300

    type inference with object literals

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,17 @@ let age = 10;
 //Arrays
 //array of strings 
 let arr = ['Ann', 'Jane', 'Hannah'];
-//arrat of objects
+console.log(arr);
+//array of objects
 let arrObj = [{ firstName: "Alice", age: 10 }];
 console.log(arrObj);
+//type inference with object literals 
+let person = {
+    name: "Jane Doe",
+    id: 1,
+    isAdmin: true
+};
+person.isAdmin = false;
+person.id = 3;
+person.name = "john doe";
+console.log(person);
