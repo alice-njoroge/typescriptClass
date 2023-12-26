ac6da07e516bc9898ec52c0d99e512ff81bbebbc
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-26 22:05:14 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let names = "Joseph";
+names = "hohoho";
+let age = 10;
+//Arrays
+//array of strings 
+let arr = ['Ann', 'Jane', 'Hannah'];
+//arrat of objects
+let arrObj = [{ firstName: "Alice", age: 10 }];
+console.log(arrObj);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let names = "Joseph";
+
+names = "hohoho"
+let age = 10;
+
+
+//Arrays
+//array of strings 
+let arr: string[] = ['Ann', 'Jane', 'Hannah'];
+
+//arrat of objects
+let arrObj : {firstName: string, age: number}[] = [{firstName: "Alice", age: 10}]
+
+console.log(arrObj);
