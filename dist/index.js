427e6e079e14f898e6152629090579b4c9f9d68b
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-12-30 07:49:15 +0300

    typescript interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,16 @@
 "use strict";
-//tuples in typescript 
-let hsla;
-hsla = [200, '100%', '100%', 1];
-let coords;
-coords = [20.1, 97.4];
-function useCoordinates() {
-    //some logic to get the coordinates 
-    const latitude = 10;
-    const longitude = 50;
-    return [latitude, longitude];
+//defining a variable of type Author 
+const authorOne = { name: 'Alice', avatar: './images/alice.png' };
+console.log(authorOne);
+const newPost = {
+    title: 'My Blog',
+    body: "my super lorem ipsum body",
+    tags: ['gaming', 'tech'],
+    created_At: new Date,
+    author: authorOne
+};
+//using interfaces as function arguments 
+function createPost(post) {
+    console.log(`${post.title} was created by ${post.author.name} `);
 }
-const [lat, long] = useCoordinates();
-//named tuples 
-let user;
-user = ['name', 27];
-console.log(user[0]);
+createPost(newPost);
